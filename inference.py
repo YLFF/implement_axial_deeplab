ea3f5765802403e74c93b4e36fa2b66b6756508c
Zhilin Wang <67310069+YLFF@users.noreply.github.com>
2021-06-03 21:49:47 +0900

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,46 @@
+# Some basic setup:
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+from google.colab.patches import cv2_imshow
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+from detectron2.data import  build_detection_train_loader
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, launch
+from detectron2.evaluation import (
+    CityscapesInstanceEvaluator,
+    CityscapesSemSegEvaluator,
+    COCOEvaluator,
+    COCOPanopticEvaluator,
+    DatasetEvaluators,)
+from detectron2.projects.panoptic_deeplab import (
+    PanopticDeeplabDatasetMapper,
+    add_panoptic_deeplab_config,
+)
+
+
+im = cv2.imread("test.png")
+#cv2_imshow(im)
+
+cfg = get_cfg()
+add_panoptic_deeplab_config(cfg)
+
+cfg.merge_from_file("./configs./Attention_PanopticDeepLab.yaml")
+
+cfg.MODEL.WEIGHTS = ("./model/axial5.pkl")
+predictor = DefaultPredictor(cfg)
+panoptic_seg, segments_info = predictor(im)["panoptic_seg"]
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+out = v.draw_panoptic_seg_predictions(panoptic_seg.to("cpu"), segments_info)
+cv2_imshow(out.get_image()[:, :, ::-1])
